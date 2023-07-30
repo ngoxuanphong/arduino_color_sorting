bda7d5e06dff4ac901ed8219d18bf86922cbc85b
ngoxuanphong <phong@cht.edu.vn>
2023-07-30 10:27:10 +0700

    update data

--- src/Color_Sorting_Arduino.ino
+++ src/Color_Sorting_Arduino.ino
@@ -1,33 +1,37 @@
 #include <Servo.h>
-#define S0 2
-#define S1 3
-#define S2 4
-#define S3 5
-#define sensorOut 6
-Servo microServo;
-int frequency = 0;
-int color=0;
-String dulieu= "";
-int myList[] = {0, 0, 0, 0, 0};
-int listSize = sizeof(myList) / sizeof(myList[0]);
+#define S0 2 // RGB sensor
+#define S1 3 // RGB sensor
+#define S2 4 // RGB sensor
+#define S3 5 // RGB sensor
+#define sensorOut 6 // RGB sensor
+Servo microServo; // Khai báo biến servo
 
-const int dirPin = 12; // Cam
-const int stepPin = 13; // Tim
+int frequency = 0; // Đọc tần số
+int color=0; // Biến lưu màu
+String dulieu= ""; // Biến lưu dữ liệu từ máy tính
+int myList[] = {0, 0, 0, 0, 0}; // Mảng lưu số lượng mỗi màu
+int listSize = sizeof(myList) / sizeof(myList[0]); // Lấy kích thước mảng myList
+
+const int dirPin = 12; // Dây cam, động cơ bước chân DIR
+const int stepPin = 13; // Dây Tím, động cơ bước chân DIR
 const int stepsPerRevolution = 200;
 
 void setup() {
+    // Setting RGB sensor pins as OUTPUT, INPUT
     pinMode(S0, OUTPUT);
     pinMode(S1, OUTPUT);
     pinMode(S2, OUTPUT);
     pinMode(S3, OUTPUT);
     pinMode(sensorOut, INPUT);
-    // Setting frequency-scaling to 20%
+
     digitalWrite(S0, HIGH);
     digitalWrite(S1, LOW);
     
+    // Cài đặt servo vào chân 9
     microServo.attach(9);
     Serial.begin(9600);
 
+    // Cài đặt STEP và DIR của động cơ bước
     pinMode(stepPin, OUTPUT);
     pinMode(dirPin, OUTPUT);
     digitalWrite(dirPin, LOW); //STEP motor (Low ngược chiều, Hight cùng chiều)
@@ -35,7 +39,7 @@ void setup() {
 
 int runcode() {
     
-//     Spin motor slowly
+    //Quay đôngj cơ bước
     for(int x = 0; x < 25; x++)
     {
         digitalWrite(stepPin, HIGH);
@@ -43,41 +47,46 @@ int runcode() {
         digitalWrite(stepPin, LOW);
         delayMicroseconds(5000);
     }
-    delay(1000); // Wait a second
+    delay(1000); // Delay 1s
   
     switch (color) {
       case 1:
-      microServo.write(40);
+      microServo.write(40); // Góc quay của servo màu xanh lá
       break;
       case 2:
-      microServo.write(70);
+      microServo.write(70); // Góc quay của servo màu đỏ
       break;
       case 3:
-      microServo.write(100);
+      microServo.write(100); // Góc quay của servo màu vàng
       break;
       case 4:
-      microServo.write(130);
+      microServo.write(130); // Góc quay của servo màu xanh dương
       break;
       case 0:
-      microServo.write(0);
+      microServo.write(0); // Góc quay của servo màu không xác định
       break;
   }
-  myList[color] += 1;
-  if (myList[color] == 100){
+  myList[color] += 1; // Tăng số lượng màu lên 1
+
+  // Nếu số lượng màu đạt 100 thì reset về 0
+  if (myList[color] == 100){ 
     myList[color] = 0;
   }
-  color=0;
-  delay(500);
+  color=0; // Reset màu về 0
+  delay(500); // Delay 0.5s
 }
 
+// Hàm loop
 void loop() {
-  String dulieu_temp= Serial.readString();
+  String dulieu_temp= Serial.readString(); // Đọc dữ liệu từ máy tính
   if (dulieu_temp != "") {
     dulieu = dulieu_temp;
   }
   
-  color = readColor();
-  Serial.print(color);
+  color = readColor(); // Gọi hàm đọc màu
+
+  // In ra màn hình màu đọc được
+  Serial.print(color); 
   Serial.print("---");
   for (int i = 0; i < listSize; i++) {
     if (myList[i] < 10){
@@ -91,6 +100,7 @@ void loop() {
   Serial.println("");
   delay(10);  
     
+  // Nếu nhận được dữ liệu từ máy tính thì chạy hàm runcode()
   if(dulieu.startsWith("B")) { 
     runcode();
   }
@@ -100,61 +110,56 @@ void loop() {
       }
     }
 }
-// Custom Function - readColor()
+
 
 int readColor() {
-  // Setting red filtered photodiodes to be read
+  // Đọc màu đỏ
   digitalWrite(S2, LOW);
   digitalWrite(S3, LOW);
-  
-  // Reading the output frequency
   frequency = pulseIn(sensorOut, LOW);
   int R = frequency;
   delay(50);
 
   
-  // Setting Green filtered photodiodes to be read
+  // Đọc màu xanh lá
   digitalWrite(S2, HIGH);
   digitalWrite(S3, HIGH);
-  // Reading the output frequency
   frequency = pulseIn(sensorOut, LOW);
   int G = frequency;
   delay(50);
 
   
-  // Setting Blue filtered photodiodes to be read
+  // Đọc màu xanh dương
   digitalWrite(S2, LOW);
   digitalWrite(S3, HIGH);
-  // Reading the output frequency
   frequency = pulseIn(sensorOut, LOW);
   int B = frequency;
 
-  
-//   Printing the value on the serial monitor
-  Serial.print("R= ");//printing name
-  Serial.print(R);//printing RED color frequency
+  // In ra màn hình giá trị RGB
+  Serial.print("R= ");
+  Serial.print(R); //Màu đỏ
   Serial.print("  ");
-  Serial.print("G= ");//printing name
-  Serial.print(G);//printing RED color frequency
+  Serial.print("G= ");
+  Serial.print(G); //Màu xanh lá
   Serial.print("  ");
-  Serial.print("B= ");//printing name
-  Serial.print(B);//printing RED color frequency
+  Serial.print("B= ");
+  Serial.print(B); //Màu xanh dương
   Serial.println("  ");
 
   delay(50);
 
   
   if( R>240 & R<270 & G>200 & G<240  & B > 250 & B< 290){
-    color = 1; // green // xanh la
+    color = 1; // green, xanh la
   }
   if(R>190 & R<220 & G>330 & G < 360 & B > 290 & B< 330){
-    color = 2; // red 
+    color = 2; // red, đỏ
   }
   if(R>100 & R <130& G>150 & G < 190 & B > 200 & B<240){
-    color = 3; // yellow
+    color = 3; // yellow, vàng
   }
   if(R > 300 & R<390 & G > 200 & G< 400 & B>250 & B<330){
-    color = 4; // blue // xanh da troi
+    color = 4; // blue, xanh da troi
   }
   return color;  
 }
